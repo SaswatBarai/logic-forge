08623dfae5b2884af963ac6d767ff0e87ad36601
Saswat Barai <saswatbarai611@gmail.com>
2026-02-19 00:28:55 +0530

    Update pnpm-lock.yaml and package.json to include new dependencies for NextAuth.js and Prisma adapter. Add auth.ts file for authentication setup using NextAuth with Google and GitHub providers.

---
+++ apps/web/auth.ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import {authDb} from "@logicforge/auth"
